3a409789a353462a2a1ee2c464a6939d7d026c5f
lucaslgr <lucaslgr1206@gmail.com>
2021-11-08 20:47:21 -0300

    Primeiro commit, setando o projeto

---
+++ src/main/java/com/squad9/bluebank/BlueBankApplication.java
@@ -0,0 +1,13 @@
+package com.squad9.bluebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/squad9/bluebank/BlueBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad9.bluebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
