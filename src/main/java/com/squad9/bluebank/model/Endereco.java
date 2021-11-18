11ebfab7f20db60d740d93502c7cbd69a75da3ab
Maxwel <maxwelfcoelho@gmail.com>
2021-11-18 13:11:03 -0300

    create: endereço model

---
+++ src/main/java/com/squad9/bluebank/model/Endereco.java
@@ -0,0 +1,127 @@
+package com.squad9.bluebank.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Table(name = "enderecos")
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_endereco", nullable = false, columnDefinition = "INT UNSIGNED")
+    private int id;
+
+    @Pattern(regexp = "^\\d{5}-\\d{3}$", message = "CEP inválido")
+    @Column(name = "cep", nullable = false, length = 9)
+    private String cep;
+
+    @NotBlank(message = "O campo logradouro é obrigatório")
+    @Column(name = "logradouro", nullable = false, length = 255)
+    private String logradouro;
+
+    @NotBlank(message = "O campo bairro é obrigatório")
+    @Column(name = "bairro", nullable = false, length = 100)
+    private String bairro;
+
+    @NotBlank(message = "O campo cidade é obrigatório")
+    @Column(name = "cidade", nullable = false, length = 100)
+    private String cidade;
+
+    @Column(name = "estado", nullable = false, length = 2)
+    @Enumerated(EnumType.STRING)
+    private Estado estado;
+
+    @Pattern(regexp = "^\\d+$", message = "Número da casa inválido")
+    @Column(name = "numero_casa", nullable = false, length = 10)
+    private String numeroCasa;
+
+    @Column(name = "complemento", length = 100)
+    private String complemento;
+
+    // todo: implementar o relacionamento com o cliente
+
+    public Endereco() {}
+
+    public Endereco(String cep, String logradouro, String bairro, String cidade, Estado estado, String numeroCasa, String complemento) {
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.numeroCasa = numeroCasa;
+        this.complemento = complemento;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCep() {
+        return this.cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return this.logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return this.bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return this.cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Estado getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public String getNumeroCasa() {
+        return this.numeroCasa;
+    }
+
+    public void setNumeroCasa(String numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getComplemento() {
+        return this.complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
