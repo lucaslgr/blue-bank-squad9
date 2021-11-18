af151286a4cc4a3cbac21963b2f5ba76f48bb94d
lucaslgr <lucaslgr1206@gmail.com>
2021-11-18 10:26:37 -0300

    Implementa atualizacao do script que gera o banco adicionado unique key composta em agencia+numero da tabela contas cc

--- db/db_bancoblue.sql
+++ db/db_bancoblue.sql
@@ -1,28 +1,28 @@
 DROP
-DATABASE IF EXISTS db_bancoblue;
+    DATABASE IF EXISTS db_bancoblue;
 CREATE
-DATABASE db_bancoblue;
+    DATABASE db_bancoblue;
 
 USE
-db_bancoblue;
+    db_bancoblue;
 
 CREATE TABLE clientes
 (
     id_cliente         INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
-    nome               VARCHAR(100) NOT NULL,
-    sobrenome          VARCHAR(100) NOT NULL,
-    cpf                VARCHAR(14)  NOT NULL,
-    rg                 VARCHAR(13)  NOT NULL,
-    data_de_nascimento DATE         NOT NULL,
-    email              VARCHAR(150) NOT NULL,
-    celular            VARCHAR(15)  NOT NULL,
+    nome               VARCHAR(100)                    NOT NULL,
+    sobrenome          VARCHAR(100)                    NOT NULL,
+    cpf                VARCHAR(14)                     NOT NULL,
+    rg                 VARCHAR(13)                     NOT NULL,
+    data_de_nascimento DATE                            NOT NULL,
+    email              VARCHAR(150)                    NOT NULL,
+    celular            VARCHAR(15)                     NOT NULL,
     telefone           VARCHAR(15),
-    senha              VARCHAR(255) NOT NULL,
+    senha              VARCHAR(255)                    NOT NULL,
     nome_do_pai        VARCHAR(255),
-    nome_da_mae        VARCHAR(255) NOT NULL,
-    profissao          VARCHAR(100) NOT NULL,
-    renda_mensal       BIGINT UNSIGNED NOT NULL,
-    patrimonio         BIGINT       NOT NULL,
+    nome_da_mae        VARCHAR(255)                    NOT NULL,
+    profissao          VARCHAR(100)                    NOT NULL,
+    renda_mensal       BIGINT UNSIGNED                 NOT NULL,
+    patrimonio         BIGINT                          NOT NULL,
 
     PRIMARY KEY (id_cliente),
     UNIQUE KEY (cpf),
@@ -33,14 +33,14 @@ CREATE TABLE clientes
 CREATE TABLE enderecos
 (
     id_endereco INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
-    cep         VARCHAR(9)   NOT NULL,
-    logradouro  VARCHAR(255) NOT NULL,
-    bairro      VARCHAR(100) NOT NULL,
-    cidade      VARCHAR(100) NOT NULL,
-    estado      VARCHAR(2)   NOT NULL,
-    numero_casa VARCHAR(10)  NOT NULL,
+    cep         VARCHAR(9)                      NOT NULL,
+    logradouro  VARCHAR(255)                    NOT NULL,
+    bairro      VARCHAR(100)                    NOT NULL,
+    cidade      VARCHAR(100)                    NOT NULL,
+    estado      VARCHAR(2)                      NOT NULL,
+    numero_casa VARCHAR(10)                     NOT NULL,
     complemento VARCHAR(100),
-    id_cliente  INTEGER UNSIGNED NOT NULL,
+    id_cliente  INTEGER UNSIGNED                NOT NULL,
 
     PRIMARY KEY (id_endereco)
 );
@@ -53,7 +53,7 @@ CREATE TABLE contas_corrente
     data_criacao DATETIME DEFAULT NOW(),
     senha        VARCHAR(255),
     saldo        BIGINT   DEFAULT 0,
-    id_cliente   INTEGER UNSIGNED NOT NULL,
+    id_cliente   INTEGER UNSIGNED                NOT NULL,
 
     PRIMARY KEY (id_conta)
 );
@@ -61,14 +61,18 @@ CREATE TABLE contas_corrente
 CREATE TABLE transacoes
 (
     id_transacao       INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
-    valor              BIGINT UNSIGNED NOT NULL,
-    id_conta_emissora  INTEGER UNSIGNED NOT NULL,
-    id_conta_receptora INTEGER UNSIGNED NOT NULL,
-    data_envio         DATETIME NOT NULL,
-    data_recebimento   DATETIME NOT NULL,
+    valor              BIGINT UNSIGNED                 NOT NULL,
+    id_conta_emissora  INTEGER UNSIGNED                NOT NULL,
+    id_conta_receptora INTEGER UNSIGNED                NOT NULL,
+    data_envio         DATETIME                        NOT NULL,
+    data_recebimento   DATETIME                        NOT NULL,
 
     PRIMARY KEY (id_transacao)
 );
+
+-- SETANDO A UNIQUE KEY AGENCIA + NUMERO
+CREATE UNIQUE INDEX conta_agencia_numero
+    ON contas_corrente (agencia, numero);
 
 -- SETANDO AS REFERENCIAS DE FOREIGN KEYS
 ALTER TABLE db_bancoblue.enderecos
