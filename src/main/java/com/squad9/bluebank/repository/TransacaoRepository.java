2280ff2e35d4804d4dba57f86014f67ea5366d1a
ayanara <aya.nathane@gmail.com>
2021-11-19 22:24:10 -0300

    Cria Repository transação

---
+++ src/main/java/com/squad9/bluebank/repository/TransacaoRepository.java
@@ -0,0 +1,9 @@
+package com.squad9.bluebank.repository;
+
+import com.squad9.bluebank.model.Transacoes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransacaoRepository extends JpaRepository <Transacoes,Long> {
+}
