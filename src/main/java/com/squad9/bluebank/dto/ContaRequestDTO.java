735643a5a08d50abca75ba04b92a8a3567858947
ayanara <aya.nathane@gmail.com>
2021-11-20 15:00:08 -0300

    Cria Conta Request DTO

---
+++ src/main/java/com/squad9/bluebank/dto/ContaRequestDTO.java
@@ -0,0 +1,23 @@
+package com.squad9.bluebank.dto;
+
+public class ContaRequestDTO {
+
+    private String cpf;
+    private String senha;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
