a183ac13dc518ada35600042daf1d8aeecd2896c
Maxwel <maxwelfcoelho@gmail.com>
2021-11-20 15:15:11 -0300

    criar endereco request dto

---
+++ src/main/java/com/squad9/bluebank/dto/EnderecoRequestDTO.java
@@ -0,0 +1,75 @@
+package com.squad9.bluebank.dto;
+
+import com.squad9.bluebank.model.Estado;
+
+public class EnderecoRequestDTO {
+    private String cep;
+
+    private String logradouro;
+
+    private String bairro;
+
+    private String cidade;
+
+    private Estado estado;
+
+    private String numeroCasa;
+
+    private String complemento;
+
+    public String getCep() {
+        return this.cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return this.logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return this.bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return this.cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Estado getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public String getNumeroCasa() {
+        return this.numeroCasa;
+    }
+
+    public void setNumeroCasa(String numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getComplemento() {
+        return this.complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }    
+}
