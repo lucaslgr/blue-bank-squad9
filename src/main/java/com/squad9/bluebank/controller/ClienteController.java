ec24469c70137eb03186b2dccfa667f85858445a
João Victor Schiavon <schiavonj@yahoo.com.br>
2021-11-25 17:49:53 -0300

    Possível verificação se cliente já possui conta

--- src/main/java/com/squad9/bluebank/controller/ClienteController.java
+++ src/main/java/com/squad9/bluebank/controller/ClienteController.java
@@ -117,6 +117,8 @@ public class ClienteController {
     @PostMapping(value = "/{idCLiente}/conta")
     public ResponseEntity<?> criarConta(@RequestBody @Valid ContaRequestDTO contaRequestDTO) {
         try {
+            // Verifica se o cliente já possui conta
+            // if (contaRequestDTO.getCpf() != null) throw new Exception ("Cliente já possui conta");
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body(contaService.cadastrarNovaConta(contaRequestDTO));
         } catch (Exception error) {
