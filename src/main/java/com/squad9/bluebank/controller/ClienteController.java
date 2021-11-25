889115943cd6cfbab9f4072591632cc4fd9bf0ab
Yuri <yurirennan14@gmail.com>
2021-11-24 22:19:58 -0300

    Cadastro Cliente criado com validações

--- src/main/java/com/squad9/bluebank/controller/ClienteController.java
+++ src/main/java/com/squad9/bluebank/controller/ClienteController.java
@@ -1,51 +1,63 @@
 package com.squad9.bluebank.controller;
 
 import com.squad9.bluebank.dto.*;
+import com.squad9.bluebank.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
-@RequestMapping("api/cliente")
+@RequestMapping("api/clientes")
 @RestController
 public class ClienteController {
+    private ClienteService clienteService;
+
+    @Autowired
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
 
     @PostMapping
-    public void criarCliente(@RequestBody ClienteRequestDTO clienteRequestDTO) throws Exception{
-        //Apagar comentário e fazer implementação
+    public ResponseEntity<ClienteResponseDTO> criarCliente(@RequestBody @Valid ClienteRequestDTO clienteRequestDTO) throws Exception {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(clienteService.salvarCliente(clienteRequestDTO));
     }
 
-    @GetMapping
-    public List<ClienteResponseDTO> listarTodosClientes() throws Exception{
-        //Apagar comentário e fazer implementação
-    }
-
-    @GetMapping(value = "/{idCliente}")
-    public ClienteResponseDTO verDadosDoCliente(@PathVariable Long idCliente) throws Exception{
-        //Apagar comentário e fazer implementação
-    }
-
-    @PutMapping(value = "/{idCliente}")
-    public void atualizarDadosDoCliente(@PathVariable Long idCliente) throws Exception{
-        //Apagar comentário e fazer implementação
-    }
-
-    @DeleteMapping
-    public void deletarCliente(@PathVariable Long idCliente) throws Exception{
-        //Apagar comentário e fazer implementação
-    }
-
-    @GetMapping(value = "/{idCliente}/transacoes")
-    public List<TransacaoResponseDTO> verHistoricoTransacoesDaContaDoCliente(@PathVariable Long idCliente) throws Exception{
-        //Apagar comentário e fazer implementação
-    }
-
-    @PostMapping(value = "/{idCliente}/transacao")
-    public void realizarTransacao(@PathVariable Long idCliente, @RequestBody TransacaoRequestDTO transacaoRequestDTO) throws Exception{
-        //Apagar comentário e fazer implementação
-    }
-
-    @PostMapping(value = "/{idCliente}/endereco")
-    public void cadastrarEndereco(@PathVariable Long idCliente, @RequestBody EnderecoRequestDTO enderecoRequestDTO) throws Exception{
-        //Apagar comentário e fazer implementação
-    }
+//    @GetMapping
+//    public List<ClienteResponseDTO> listarTodosClientes() throws Exception{
+//        //Apagar comentário e fazer implementação
+//    }
+//
+//    @GetMapping(value = "/{idCliente}")
+//    public ClienteResponseDTO verDadosDoCliente(@PathVariable Long idCliente) throws Exception{
+//        //Apagar comentário e fazer implementação
+//    }
+//
+//    @PutMapping(value = "/{idCliente}")
+//    public void atualizarDadosDoCliente(@PathVariable Long idCliente) throws Exception{
+//        //Apagar comentário e fazer implementação
+//    }
+//
+//    @DeleteMapping
+//    public void deletarCliente(@PathVariable Long idCliente) throws Exception{
+//        //Apagar comentário e fazer implementação
+//    }
+//
+//    @GetMapping(value = "/{idCliente}/transacoes")
+//    public List<TransacaoResponseDTO> verHistoricoTransacoesDaContaDoCliente(@PathVariable Long idCliente) throws Exception{
+//        //Apagar comentário e fazer implementação
+//    }
+//
+//    @PostMapping(value = "/{idCliente}/transacao")
+//    public void realizarTransacao(@PathVariable Long idCliente, @RequestBody TransacaoRequestDTO transacaoRequestDTO) throws Exception{
+//        //Apagar comentário e fazer implementação
+//    }
+//
+//    @PostMapping(value = "/{idCliente}/endereco")
+//    public void cadastrarEndereco(@PathVariable Long idCliente, @RequestBody EnderecoRequestDTO enderecoRequestDTO) throws Exception{
+//        //Apagar comentário e fazer implementação
+//    }
 }
