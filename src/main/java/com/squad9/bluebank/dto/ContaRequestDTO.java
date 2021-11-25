bed457f6692313c04fd39b811b1101ea9658b533
lucaslgr <lucaslgr1206@gmail.com>
2021-11-24 22:21:20 -0300

    Implementa validacoes do DTO Request

--- src/main/java/com/squad9/bluebank/dto/ContaRequestDTO.java
+++ src/main/java/com/squad9/bluebank/dto/ContaRequestDTO.java
@@ -1,8 +1,18 @@
 package com.squad9.bluebank.dto;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class ContaRequestDTO {
 
+    @NotBlank(message = "O campo cpf não pode estar vazio.")
+    @CPF(message = "CPF inválido.")
     private String cpf;
+
+    @NotBlank(message = "O campo senha não pode estar vazio.")
+    @Size(min = 6, message = "A senha deve conter pelo menos 6 caracteres.")
     private String senha;
 
     public String getCpf() {
