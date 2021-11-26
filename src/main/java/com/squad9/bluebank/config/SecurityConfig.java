4e41298e92e6e6f34d328775554526fb747f5d19
Maxwel <maxwelfcoelho@gmail.com>
2021-11-26 12:17:14 -0300

    configurar a encriptogracao para usar o bcrypt

--- src/main/java/com/squad9/bluebank/config/SecurityConfig.java
+++ src/main/java/com/squad9/bluebank/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.squad9.bluebank.config;
 
 import com.squad9.bluebank.service.DetalheUsuarioServiceImpl;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,7 +21,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(detalheUsuarioService);
+        auth.userDetailsService(detalheUsuarioService).passwordEncoder(passwordEncoder());
     }
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
