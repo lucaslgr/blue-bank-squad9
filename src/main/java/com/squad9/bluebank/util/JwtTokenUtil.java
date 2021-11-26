784fd725131d02c32553bce09556f7738d009507
Maxwel <maxwelfcoelho@gmail.com>
2021-11-26 15:22:08 -0300

    anotar como bean

--- src/main/java/com/squad9/bluebank/util/JwtTokenUtil.java
+++ src/main/java/com/squad9/bluebank/util/JwtTokenUtil.java
@@ -6,9 +6,11 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtTokenUtil {
     
-
     public String gerarToken(String email) throws Exception {
         try {
             String token = JWT.create()
