4a5bf36987812449b7c2097c769916ad6f016d7c
ayanara <aya.nathane@gmail.com>
2021-12-03 16:40:19 -0300

    Propriedade Ambiente SNS

--- src/main/java/com/squad9/bluebank/service/SNSServiceImpl.java
+++ src/main/java/com/squad9/bluebank/service/SNSServiceImpl.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sns.model.SubscribeRequest;
 import com.squad9.bluebank.dto.ClienteResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -19,7 +20,8 @@ public class SNSServiceImpl implements SNSService {
     }
 
     //Tópico para informar o cadastro de novos clientes
-    private final String TOPIC_ARN = "";
+    @Value( "${cloud.aws.arnsns}" )
+    private String TOPIC_ARN;
 
     public void addSubscricao(@PathVariable String email) {
         SubscribeRequest subscribeRequest = new SubscribeRequest(TOPIC_ARN, "email", email);
